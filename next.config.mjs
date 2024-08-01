9f3df9fac653bc0738ed23a1fce168fe649a2da5
Sivamani-18 <siva@mindwaveventures.com>
2024-08-01 17:45:02 +0530

    Deploy to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/awesome-github-repos' : '', // Use an empty string for development
+  assetPrefix: isProd
+    ? 'https://sivamani-18.github.io/awesome-github-repos/'
+    : undefined,
+  trailingSlash: true,
+  publicRuntimeConfig: {
+    basePath: isProd ? '/awesome-github-repos' : '',
+  },
+};
 
 export default nextConfig;
